f6c60c5c87a77abc951cdf2a0af9a04ce6433386
Nguyen Quang Viet <quangvietds3@gmail.com>
2016-09-13 11:12:02 +0700

    display project
    Giao dien ban đầu

---
+++ VnExpress_New/app/src/main/java/com/example/quang/vnexpress_new/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.quang.vnexpress_new;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.trangchu) {
+            // Handle the camera action
+        } else if (id == R.id.thoisu) {
+
+        } else if (id == R.id.thegioi) {
+
+        } else if (id == R.id.kinhdoanh) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+
+        if (id == R.id.giaitri) {
+            // Handle the camera action
+        } else if (id == R.id.thethao) {
+
+        } else if (id == R.id.phapluat) {
+
+        } else if (id == R.id.giaoduc) {
+
+        } else if (id == R.id.suckhoe) {
+
+        } else if (id == R.id.giadinh) {
+
+        }
+
+
+        if (id == R.id.dulich) {
+            // Handle the camera action
+        } else if (id == R.id.khoahoc) {
+
+        } else if (id == R.id.sohoa) {
+
+        } else if (id == R.id.xe) {
+
+        } else if (id == R.id.congdong) {
+
+        } else if (id == R.id.tamsu) {
+
+        }
+
+        if (id == R.id.cuoi) {
+            // Handle the camera action
+        } else if (id == R.id.video) {
+
+        } else if (id == R.id.raovat) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ VnExpress_New/app/src/main/java/com/example/quang/vnexpress_new/display_new_vnexpress.java
@@ -0,0 +1,13 @@
+package com.example.quang.vnexpress_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class display_new_vnexpress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_new_vnexpress);
+    }
+}
