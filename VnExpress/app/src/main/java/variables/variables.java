7da75b9194f8f255c54fa9be94452922cc65eb04
zukizaro <minhduong.zaro@gmail.com>
2016-09-20 12:33:05 +0700

    variables

---
+++ VnExpress/app/src/main/java/variables/variables.java
@@ -0,0 +1,34 @@
+package variables;
+
+/**
+ * Created by Joyboy on 9/20/2016.
+ */
+
+public class variables {
+    public static final String[] CATEGORIES = {
+            "Trang chủ", "Thời sự", "Thế giới", "Kinh doanh", "Giải trí", "Thể thao", "Pháp luật",
+            "Giáo dục", "Sức khỏe", "Gia đình", "Du lịch", "Khoa học", "Số hóa", "Xe", "Cộng đồng",
+            "Tâm sự", "Cười", "Video"
+    };
+    public static final String[] URL = {
+            "http://vnexpress.net/rss/tin-moi-nhat.rss",
+            "http://vnexpress.net/rss/thoi-su.rss",
+            "http://vnexpress.net/rss/the-gioi.rss",
+            "http://vnexpress.net/rss/kinh-doanh.rss",
+            "http://vnexpress.net/rss/giai-tri.rss",
+            "http://vnexpress.net/rss/the-thao.rss",
+            "http://vnexpress.net/rss/phap-luat.rss",
+            "http://vnexpress.net/rss/giao-duc.rss",
+            "http://vnexpress.net/rss/suc-khoe.rss",
+            "http://vnexpress.net/rss/gia-dinh.rss",
+            "http://vnexpress.net/rss/du-lich.rss",
+            "http://vnexpress.net/rss/khoa-hoc.rss",
+            "http://vnexpress.net/rss/so-hoa.rss",
+            "http://vnexpress.net/rss/oto-xe-may.rss",
+            "http://vnexpress.net/rss/cong-dong.rss",
+            "http://vnexpress.net/rss/tam-su.rss",
+            "http://vnexpress.net/rss/cuoi.rss",
+            "http://video.vnexpress.net/"
+    };
+}
+
